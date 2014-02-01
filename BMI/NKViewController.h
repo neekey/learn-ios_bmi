ae2f129b1c27dd052c137b0d5adf81fb38ce1602
yinruo.nyj <yinruo.nyj@taobao.com>
2014-02-01 19:18:29 +0800

    完成APP

--- BMI/NKViewController.h
+++ BMI/NKViewController.h
@@ -8,6 +8,37 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NKViewController : UIViewController
+@interface NKViewController : UIViewController <UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate>
+
+// picker行方的toolbar
+@property (strong, nonatomic) IBOutlet UIToolbar *heightPickerViewToolbar;
+// picker
+@property (strong, nonatomic) IBOutlet UIPickerView *pickerView;
+// 用于输入高度的文本框
+@property (strong, nonatomic) IBOutlet UITextField *heightTextField;
+// 用于输入体重的文本框
+@property (strong, nonatomic) IBOutlet UITextField *weightTextField;
+// 计算 按钮
+@property (strong, nonatomic) IBOutlet UIButton *calculate;
+// BMI结果显示label
+@property (strong, nonatomic) IBOutlet UILabel *result;
+
+// 计算按钮点击action
+- (IBAction) calculateBMI:(id)sender;
+// 工具栏上 完成 按钮
+- (IBAction) doneSelectHeight: (id)sender;
+
+// 身高当前选中的行
+@property NSInteger heightSelectRow;
+// 体重当前选中行
+@property NSInteger weightSelectRow;
+// 身高选项列表
+@property NSMutableArray* heightDataList;
+// 体重选项列表
+@property NSMutableArray* weightDataList;
+// 当前数据列表
+@property NSMutableArray* currentDataList;
+// 当前被激活的文本框
+@property UITextField* activeTextField;
 
 @end
--- BMI/NKViewController.m
+++ BMI/NKViewController.m
@@ -10,6 +10,26 @@
 
 @interface NKViewController ()
 
+/**
+ * 根据当前激活的textField来返回对应的数据
+ */
+- (NSMutableArray*) getCurrentDataListByTextField: (UITextField *)textField;
+
+/**
+ * 根据当前textField已经选中的菜单的行数
+ */
+- (NSInteger) getCurrentPickerSelectedRowByTextField: (UITextField *)textField;
+
+/**
+ * 根据当前textField的值的单位
+ */
+- (NSString*) getCurrentPickerDataUnitByTextField: (UITextField *)textField;
+
+/**
+ * 保存当前选中的行数
+ */
+- (void) setCurrentSelectRow:(NSInteger)currentSelectRow  byTextField: (UITextField *)textField;
+
 @end
 
 @implementation NKViewController
@@ -17,13 +37,116 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+
+    self.heightDataList = [NSMutableArray arrayWithCapacity: 100];
+    self.weightDataList = [NSMutableArray arrayWithCapacity: 100];
+
+    for (int i = 5; i <= 300; i++ ) {
+        [self.heightDataList addObject: [ NSString stringWithFormat: @"%d", i ]];
+    }
+
+    for (int i = 10; i <= 200; i++ ) {
+        [self.weightDataList addObject: [ NSString stringWithFormat: @"%d", i ]];
+    }
+
+    // 设置textField的代理
+    self.heightTextField.delegate = self;
+    // 设置inputView和inputAccessoryView，当textField需要输入时，会显示两者，accessory在inputView上方
+    self.heightTextField.inputView = self.pickerView;
+    self.heightTextField.inputAccessoryView = self.heightPickerViewToolbar;
+    self.weightTextField.delegate = self;
+    self.weightTextField.inputView = self.pickerView;
+    self.weightTextField.inputAccessoryView = self.heightPickerViewToolbar;
+    // 已经选中的行数
+    self.heightSelectRow = 0;
+    self.weightSelectRow = 0;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSMutableArray *)getCurrentDataListByTextField: (UITextField *)textField  {
+    return ( textField == self.heightTextField ) ?
+    self.heightDataList : self.weightDataList;
+}
+
+- (NSInteger) getCurrentPickerSelectedRowByTextField: (UITextField *)textField {
+    return (textField == self.heightTextField ) ?
+    self.heightSelectRow : self.weightSelectRow;
+}
+
+- (NSString*) getCurrentPickerDataUnitByTextField: (UITextField *)textField {
+    return self.activeTextField == self.heightTextField ? @"厘米" : @"千克" ;
+}
+
+- (void) setCurrentSelectRow:(NSInteger)currentSelectRow byTextField: (UITextField *)textField {
+
+    if ( textField == self.heightTextField ) {
+        self.heightSelectRow = currentSelectRow;
+    }
+    else {
+        self.weightSelectRow = currentSelectRow;
+    }
+}
+
+#pragma mark UITextFieldDelegate
+
+/**
+ * 菜单即将出现时
+ */
+- (BOOL) textFieldShouldBeginEditing: (UITextField *)textField {
+
+    self.activeTextField = textField;
+    self.currentDataList = [self getCurrentDataListByTextField:textField];
+
+    // 重新装载picker的数据
+    [self.pickerView reloadAllComponents];
+
+    // 选中默认值
+    [self.pickerView selectRow: [self getCurrentPickerSelectedRowByTextField:textField ] inComponent:0 animated:YES];
+
+    // 检查 textField 是否还为空，若为空，则默认选中中间的选项
+    if( [textField.text length] == 0 ){
+        [self.pickerView selectRow: [self.currentDataList count] / 2 inComponent:0 animated:YES];
+    }
+    return YES;
+}
+
+#pragma mark actions
+
+/**
+ * 点击完成按钮
+ */
+- (void)doneSelectHeight:(id)sender {
+    NSInteger row = [self.pickerView selectedRowInComponent:0];
+    self.activeTextField.text = [[self.currentDataList objectAtIndex:row] stringByAppendingString:[ self getCurrentPickerDataUnitByTextField: self.activeTextField ]];
+    [self setCurrentSelectRow:row byTextField: self.activeTextField];
+    [self.activeTextField endEditing:YES];
+}
+
+/**
+ * 点击计算
+ */
+- (void)calculateBMI:(id)sender {
+    float height = [self.heightTextField.text floatValue];
+    float weight = [self.weightTextField.text floatValue];
+    float bmi = weight / ( height * 2 / 100 );
+
+    NSString* result = [ NSString stringWithFormat: @"%f", bmi ];
+
+    self.result.text = result;
+}
+
+#pragma mark UIPickerViewDelegate & UIPickerViewDataSource
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return [self.currentDataList count];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSString *) pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return  [[self.currentDataList objectAtIndex: row] stringByAppendingString: [ self getCurrentPickerDataUnitByTextField: self.activeTextField ] ];
 }
 
 @end
